98af4f5d02319efa2d5847db8e8d5d73f08ddffb
Tim Tyrrell <timomitchel23@gmail.com>
2020-08-25 13:14:55 -0600

    Update seeds to destroy first

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Artist.destroy_all
+Song.destroy_all
 rtj = Artist.create!(name: 'Run The Jewels')
 caamp = Artist.create!(name: 'Caamp')
 jgb = Artist.create!(name: 'Jerry Garcia Band')
