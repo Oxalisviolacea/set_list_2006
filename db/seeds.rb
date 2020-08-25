24dfc6244906c2a707e90e3e1b77a25e7b7e70ce
Tim Tyrrell <timomitchel23@gmail.com>
2020-08-25 12:36:50 -0600

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,8 @@ caamp.songs.create!(title: 'Vagabond', length: 240, play_count: 120000)
 jgb.songs.create!(title: 'Aint No Bread In The Breadbox', length: 540, play_count: 12000)
 jgb.songs.create!(title: 'The Harder They Come', length: 240, play_count: 120000)
 
-billie.songs.create!(title: 'bury a friend', length: 340, play_count: 12000000000)
-billie.songs.create!(title: 'bad guy', length: 240, play_count: 100000000)
+billie.songs.create!(title: 'bury a friend', length: 340, play_count: 1200000)
+billie.songs.create!(title: 'bad guy', length: 240, play_count: 100000)
 
 lcd.songs.create!(title: 'Someone Great', length: 500, play_count: 1000000)
 lcd.songs.create!(title: 'I Can Change', length: 640, play_count: 100000)
