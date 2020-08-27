b049f940ed83be70d0568441f31f4f43a065774e
Brian Zanti <brianzanti@gmail.com>
2020-08-27 10:05:16 -0600

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@artist_1 = Artist.create(name: 'Prince')
+@song_1 = Song.create(title: "Purple Rain",
+                      length: 384543,
+                      artist: @artist_1,
+                      play_count: 5454)
+@song_2 = Song.create(title: "Lets Go Crazy",
+                      length: 8435,
+                      artist: @artist_1,
+                      play_count: 864)
