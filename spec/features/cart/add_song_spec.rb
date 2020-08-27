d4abe958bf5fd5d39f56466417c97ba16711471c
Brian Zanti <brianzanti@gmail.com>
2020-08-27 09:33:50 -0600

    adds cart test

---
+++ spec/features/cart/add_song_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Adding a song to the cart' do
+  it 'shows a message that the song has been added' do
+    @artist_1 = Artist.create(name: 'Prince')
+    @song_1 = Song.create(title: "Purple Rain",
+                          length: 384543,
+                          artist: @artist_1,
+                          play_count: 5454)
+    @song_2 = Song.create(title: "Lets Go Crazy",
+                          length: 8435,
+                          artist: @artist_1,
+                          play_count: 864)
+
+    visit '/songs'
+    # within "#song-#{@song_1.id}" do
+    #   click_button 'Add to Cart'
+    # end
+    # all(:button, 'Add to Cart').first.click
+  end
+end
+# S - setup
+# E - exercise/execute
+# A - assertion
+#
+#
+# T - teardown
